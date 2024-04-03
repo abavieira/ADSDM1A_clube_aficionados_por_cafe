8e987ff66a908716432974b323aaf56e38d5218c
Pamanix <pahrodr@gmail.com>
2024-04-03 10:26:01 -0300

    atualizando

--- script.js
+++ script.js
@@ -8,28 +8,27 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-// Tipos de grãos
+// Texto
 
-// Seleciona todos os elementos h4 com a classe 'tipos'
+// Seleciona todos os elementos h4 de 'tipos'
 var h4Elements = document.querySelectorAll('h4.tipos');
 
-// Para cada elemento h4, adiciona um evento de mouse
+// Para cada h4 adiciona um evento de mouse
 h4Elements.forEach(function(h4) {
-  // Adiciona um ouvinte de evento de mouse para o evento 'mouseover'
+
   h4.addEventListener('mouseover', function() {
-    // Seleciona o próximo elemento irmão com a classe 'texto'
+    // Seleciona o próximo elemento com a classe 'texto'
     var textoElement = this.nextElementSibling;
 
-    // Adiciona a classe 'mostrar' ao elemento irmão
+    // Adiciona a classe 'mostrar'
     textoElement.classList.add('mostrar');
   });
 
-  // Adiciona um ouvinte de evento de mouse para o evento 'mouseout'
   h4.addEventListener('mouseout', function() {
     // Seleciona o próximo elemento irmão com a classe 'texto'
     var textoElement = this.nextElementSibling;
 
-    // Remove a classe 'mostrar' do elemento irmão
+    // Remove a classe 'mostrar' 
     textoElement.classList.remove('mostrar');
   });
 
